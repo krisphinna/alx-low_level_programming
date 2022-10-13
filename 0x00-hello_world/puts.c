e730019afd0fa2bb7b62919bdb98092ba2b732e0
krisphinna <roykriz@yahoo.com>
2022-10-13 13:50:40 -0700

    puts

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * Use the function puts
+ *
+ * Description: print "Programming is like building a multilingual puzzle"
+ *
+ * return the value 0
+ */
+
+int main(void) 
+{
+	puts("Programming is like building a multilingual puzzle/n");
+	return (0);
+}
