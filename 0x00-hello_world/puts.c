c187eece0d865474778fd48e0211fb69088869b7
krisphinna <roykriz@yahoo.com>
2022-10-14 02:20:22 -0700

    puts

--- 0x00-hello_world/puts.c
+++ 0x00-hello_world/puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
-/*
- * Use the function puts
+/**
+ * main - Entry point
  *
- * Description: print "Programming is like building a multilingual puzzle"
- *
- * return the value 0
+ * Return: Always 0 (Success)
  */
-
-int main(void) 
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle/n");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
-}
+};
