430353851096bd6b8467575c113d476880d966b7
krisphinna <roykriz@yahoo.com>
2022-10-14 15:48:49 -0700

    when i cant sleep i play alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low_case;
+
+	for (low_case = 'a'; low_case <= 'z'; low_case++)
+		putchar(low_case);
+		putchar('\n');
+		return (0);
+}
