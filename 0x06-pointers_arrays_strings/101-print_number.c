7aec798dd19710fde2985269b567a6e8b87028f3
krisphinna <roykriz@yahoo.com>
2022-10-27 14:51:33 -0700

    task 101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer
+ * @n: input number
+ * Return: 0 Always
+ */
+
+void print_number(int n)
+{
+	if (n > -1)
+	{
+		if (n > 9)
+			print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		_putchar('-');
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
+	}
+}
