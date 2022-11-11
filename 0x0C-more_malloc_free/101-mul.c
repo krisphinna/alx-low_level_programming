a31c65aa2b905e98c52a4eeca22e43dfeb995e31
krisphinna <roykriz@yahoo.com>
2022-11-11 12:54:23 -0800

    task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,6 +3,50 @@
 #include <stdio.h>
 
 /**
+ * is_digit - check if a string contain non-digit char
+ * @s: string evaluation
+ *
+ * Return: 0 always
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - a function to return a string length
+ * @s: string evaluation
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * errors - handle errors
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
  * main - a program that multiplies two positive numbers.
  * @argc: argument count
  * @argv: argument value
@@ -10,19 +54,42 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, mul;
+	char *s1, *s2;
+	int len1, len2, len, mul, num1, num2, i, a = 0, *res;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	res = malloc(sizeof(int) * len);
+	if (!res)
+		return (1);
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("Error\n");
-		exit(98);
+		num1 = s1[len1] - '0';
+		mul = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			num2 = s2[len2] - '0';
+			mul += res[len1 + len2 + 1] + (num1 * num2);
+			res[len1 + len2 + 1] = mul % 10;
+			mul /= 10;
+		}
+		if (mul > 0)
+			res[len1 + len2 + 1] += mul;
 	}
-	else
+	for (i = 0; i < len - 1; i++)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		mul = num1 * num2;
-		printf("%d\n", mul);
-		return (0);
+		if (res[i])
+			a = 1;
+		if (a)
+			_putchar(res[i] + '0');
 	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(res);
+	return (0);
 }
